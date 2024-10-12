6b1602bbc329ee8b3e4bfbccd7f52b75d9df17a4
Wangyunbo-wyb <3437108157@qq.com>
2024-10-12 22:28:22 +0800

    update

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -15,7 +15,13 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input){
+        Ok(cost) => cost,
+        Err(e) => {
+            println!("Failed to parse item quantity.");
+            return;
+        }
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
